e4d005e2887ae1ed06966ac218afe8cdd33ed83f
taiseee <taisei7548@gmail.com>
2023-10-29 01:41:17 +0900

    delete:response型削除

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,14 +1,6 @@
 from typing import List
 from pydantic import BaseModel, Field
 
-class UserData(BaseModel):
-    id: int = Field(None, discription="ユーザーID")
-    name: str = Field(None, discription="ユーザー名")
-    sex: int = Field(None, discription="性別")
-    personality: str = Field(None, discription="性格")
-    hobby: str = Field(None, discription="趣味")
-    line_url: str = Field(None, discription="LINEのURL")
-
 class RegisterRequest(BaseModel):
     name: str = Field(..., discription="ユーザー名")
     password: str = Field(..., discription="password")
@@ -18,9 +10,3 @@ class RegisterRequest(BaseModel):
     self_introduction: str = Field(None, discription="自己紹介")
     line_url: str = Field(..., discription="LINEのURL")
     image_url: str = Field(None, discription="画像ファイル")
-
-class RegisterResponse(BaseModel):
-    id: int = Field(None, discription="ユーザーID")
-
-class RecommendationRequest(BaseModel):
-    id: int = Field(None, discription="ユーザーID")
